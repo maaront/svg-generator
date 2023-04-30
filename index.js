fc5c4a5a425ce77b9d701a09b6b5a411ee4810e7
maaront <35611834+maaront@users.noreply.github.com>
2023-04-30 12:23:01 -0400

    populate svg document with responses

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// Circle
-<svg version="1.1"
-     width="300" height="200"
-     xmlns="http://www.w3.org/2000/svg">
-
-  <circle r="80" fill="green" />
-
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-
-</svg>
-
-
 // Import packages
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -31,7 +19,7 @@ inquirer
     {
       type: 'input',
       message: 'Add text color (keyword or hexadecimal). Leave blank for no text.',
-      name: 'text-color',
+      name: 'textcolor',
     },
     {
       type: 'list',
@@ -46,6 +34,104 @@ inquirer
     {
         type: 'input',
         message: 'Add shape color (keyword or hexadecimal).',
-        name: 'shape-color',
+        name: 'shapecolor',
       },
 
+
+    ])
+    .then((answers) => {
+      // Generate the SVG content based on the user's answers
+      const svgContent = generateSVG(answers);
+  
+      // Write the SVG content to a file
+      fs.writeFile('logo.svg', svgContent, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Generated logo.svg');
+        }
+      });
+    });
+  
+  // Generate logo.svg from user responses
+  const generateSVG = (
+  {
+    text,
+    textcolor,
+    shape,
+    shapecolor,
+    });
+      return `
+<svg version="1.1"
+width="300" height="300"
+xmlns="http://www.w3.org/2000/svg">
+ 
+<${shape} cx="80" cy="80" r="80" fill="${shapecolor}" />
+
+<text x="80" y="100" font-size="60" text-anchor="middle" fill="${textcolor}">SVG</text>
+ 
+ </svg>
+  
+  ## License
+  ${license}
+  `
+  };
+  
+
+
+])
+.then((answers) => {
+  // Generate the README content based on the user's answers
+  const readMEContent = generateREADME(answers, answers.hasScreenshot, answers.screenshot);
+
+  // Write the README content to a file
+  fs.writeFile('README.md', readMEContent, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Successfully created README.md!');
+
+      // Prompt the user if they want to open the README.md file
+      inquirer
+        .prompt([
+          {
+            type: 'confirm',
+            message: 'Do you want to open the README.md file?',
+            name: 'openFile',
+          },
+        ])
+        .then((answer) => {
+          // Open the README.md file if the user selects 'Yes'
+          if (answer.openFile) {
+            openFile('README.md');
+          } else {
+            console.log('File not opened.');
+          }
+        });
+    }
+  });
+});
+
+// Generate README.md from user responses
+const generateREADME = (
+{
+title,
+description,
+install,
+usage,
+contribution,
+testing,
+license,
+username,
+email,
+},
+hasScreenshot,
+screenshot
+) => {
+const screenshotSection = hasScreenshot ? `\n## Screenshot\n\n![Screenshot](${screenshot})\n` : '';
+  return `
+
+
+# ${title}
+`
+};
