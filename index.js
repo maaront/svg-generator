ad592a7a5c35f76f61d2ac11cb579312490c1b78
maaront <35611834+maaront@users.noreply.github.com>
2023-04-30 13:18:45 -0400

    add switch case statement to populate svg depending on user choice

--- index.js
+++ index.js
@@ -49,21 +49,46 @@ inquirer
     });
   
   // Generate logo.svg from user responses
-  const generateSVG = (
-  {
+  const generateSVG = ({
     text,
     textcolor,
     shape,
     shapecolor,
-    });
-      return `
-<svg version="1.1"
-width="300" height="300"
-xmlns="http://www.w3.org/2000/svg">
- 
-<${shape} cx="80" cy="80" r="80" fill="${shapecolor}" />
-
-<text x="80" y="100" font-size="60" text-anchor="middle" fill="${textcolor}">SVG</text>
- 
- </svg>
-  `
+  }) => {
+    // Declare a variable to store the shape SVG element
+    let shapeElement;
+  
+    // Switch statement to handle different shape selections
+    switch (shape) {
+      case 'circle':
+        // Set the shapeElement to circle
+        shapeElement = `<circle cx="80" cy="80" r="80" fill="${shapecolor}" />`;
+        break;
+      case 'triangle':
+        // Set the shapeElement to triangle
+        shapeElement = `
+          <polygon points="80,10 150,150 10,150" fill="${shapecolor}" />
+        `;
+        break;
+      case 'square':
+        // Set the shapeElement to a rectangle
+        shapeElement = `<rect x="10" y="10" width="140" height="140" fill="${shapecolor}" />`;
+        break;
+      default:
+        // Throw an error if an invalid shape is selected
+        throw new Error('Invalid shape selected.');
+    }
+  
+    // Return the final SVG string with the shape and text elements
+    return `
+  <svg version="1.1"
+  width="300" height="300"
+  xmlns="http://www.w3.org/2000/svg">
+   
+  ${shapeElement} <!-- Insert the shape based on the user's choice -->
+  
+  <text x="80" y="100" font-size="60" text-anchor="middle" fill="${textcolor}">${text}</text> <!-- Insert the text SVG element with the user's input -->
+   
+  </svg>
+  `;
+  };
