7e04cb700aa0d3d123bac89fd43db3377396547c
maaront <35611834+maaront@users.noreply.github.com>
2023-04-30 11:42:44 -0400

    add inquirer, fswrite, and user prompts

--- index.js
+++ index.js
@@ -8,3 +8,44 @@
   <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
 
 </svg>
+
+
+// Import packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Import open package and define openFile function
+async function openFile(filepath) {
+  const open = (await import('open')).default;
+  await open(filepath);
+}
+
+// Begin user prompts using inquirer
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Add up to three characters of text. Leave blank for no text.',
+      name: 'text',
+    },
+    {
+      type: 'input',
+      message: 'Add text color (keyword or hexadecimal). Leave blank for no text.',
+      name: 'text-color',
+    },
+    {
+      type: 'list',
+      message: 'Choose a shape',
+      name: 'shape',
+      choices: [
+        'circle',
+        'triangle',
+        'square',
+      ],
+    },
+    {
+        type: 'input',
+        message: 'Add shape color (keyword or hexadecimal).',
+        name: 'shape-color',
+      },
+
