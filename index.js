8f11be249298cd191c0b6cbcd43a3c74159b00a3
maaront <35611834+maaront@users.noreply.github.com>
2023-04-30 11:22:43 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Circle
+<svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+
+  <circle r="80" fill="green" />
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+</svg>
